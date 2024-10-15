191d16040123b0339b4433217137d5f353081bad
Nebi017 <wedizere123@gmail.com>
2024-10-15 09:44:55 +0300

    Add files via upload

---
+++ Js Ass 6/jsScript.js
@@ -0,0 +1,35 @@
+ 
+ 
+   //1.Question number 1
+
+// function add (a,b){
+//     return a + b;
+// }
+
+ const add= (a,b) => a + b ;
+ console.log(add(5,5));
+
+
+ //1.Question number 2
+
+ // function multiply (a,b){
+//     return a * b;
+// }
+
+const mltiply= (a,b) => a * b ;
+ console.log(mltiply(5,5));
+
+
+//1.Question number 3
+
+ // function greet (name){
+//     return 'Hello, ' + name;
+// }
+
+const greet = name => 'Hello, ' + name;
+console.log(greet('Nebiy'));
+
+
+
+
+
