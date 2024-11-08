bbee721de98b47456b5838015b1e8fbf4005d119
Nebi017 <wedizere123@gmail.com>
2024-11-08 13:51:15 +0300

    Add files via upload

---
+++ React-1/src/App.jsx
@@ -0,0 +1,19 @@
+import Content from "./Content/content";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+
+
+
+function App() {
+  return(
+    <>
+   <Header />
+   <Content />
+   <Footer />
+    </>
+
+  );
+}
+
+
+export default App;
